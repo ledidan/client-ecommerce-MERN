29ccfde4537bf7bb7a8690fee8a9e9db56bf99cc
ledidan <didan.mobe@gmail.com>
2022-09-28 11:59:15 +0700

    add desc for contactInfo

--- src/components/homeComponents/ContactInfo.js
+++ src/components/homeComponents/ContactInfo.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function ContactInfo() {
-  return <div>ContactInfo</div>;
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h2>Call us 24/7</h2>
+            <p>053455483</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Headquarter</h5>
+            <p>Arusha Nijro Pepsi</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
