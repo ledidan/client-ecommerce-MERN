42a600f51aeb237236e78d7aee9eeb2c70cabf74
fiau <didan.mobe@gmail.com>
2022-09-30 10:00:42 +0700

    code1

---
+++ src/views/ShippingScreen.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Header from "../components/Header";
+
+const ShippingScreen = () => {
+  window.scrollTo(0, 0);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <Header />
+      <div className="container d-flex justify-content-center align-items-center login-center">
+        <form
+          className="Login col-md-8 col-lg-4 col-11"
+          onSubmit={submitHandler}
+        >
+          <h6>DELIVERY ADDRESS</h6>
+          <input type="text" placeholder="Enter address" required />
+          <input type="text" placeholder="Enter city" required />
+          <input type="text" placeholder="Enter postal code" required />
+          <input type="text" placeholder="Enter country" required />
+          <button type="submit">
+            <Link to="/payment" className="text-white">
+              Continue
+            </Link>
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ShippingScreen;
