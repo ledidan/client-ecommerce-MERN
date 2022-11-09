eb06b5fb8f527e52cce109f88318c425c175baef
ledidan <didan.mobe@gmail.com>
2022-11-09 23:37:33 +0700

    make it better

--- src/components/LoadingError/Loading.js
+++ src/components/LoadingError/Loading.js
@@ -1,16 +1,12 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Heading, Spinner, VStack } from "@chakra-ui/react";
 import React from "react";
 
 export default function Loading() {
   return (
     <VStack className="justify-content-center">
-      <Box
-        className="spinner-border text-danger"
-        role="status"
-        style={{ width: "50px", height: "50px" }}
-      ></Box>
+      <Spinner />
       <Heading as="h4" size="md">
-        Loading...
+        Đang tải...
       </Heading>
     </VStack>
   );
