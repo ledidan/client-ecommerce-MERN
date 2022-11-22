c81d452775898d4eeb4d72e70fd4fc761e7e2192
ledidan <didan.mobe@gmail.com>
2022-11-22 11:35:17 +0700

    slice 5 pros in single pro detail

--- src/views/SingleProduct.js
+++ src/views/SingleProduct.js
@@ -191,7 +191,7 @@ const SingleProduct = ({ match }) => {
           </Center>
           <div className="marquee">
             <div className="maylike-products-container track">
-              {products.map((item) => (
+              {products.slice(0, 5).map((item) => (
                 <PreferProduct key={item._id} productPrefer={item} />
               ))}
             </div>
